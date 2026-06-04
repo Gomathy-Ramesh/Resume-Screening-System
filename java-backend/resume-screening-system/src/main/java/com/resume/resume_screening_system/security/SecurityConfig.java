14ad73c5ad6739e827cf16f4eabfa6ff14fad242
Gomathy-Ramesh <gomathyramesh05@gmail.com>
2026-06-04 11:38:41 +0530

    Add home endpoint for Render

--- java-backend/resume-screening-system/src/main/java/com/resume/resume_screening_system/security/SecurityConfig.java
+++ java-backend/resume-screening-system/src/main/java/com/resume/resume_screening_system/security/SecurityConfig.java
@@ -74,6 +74,7 @@ public class SecurityConfig {
                         // =========================
 
                         .requestMatchers(
+                                "/",
 
                                 "/auth/**",
 
